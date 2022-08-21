31d6350f1d28d602e96f280b8b8855b64b36893a
Ranjit Shinde <ranjitshinde9404@gmail.com>
2022-08-21 18:24:00 +0530

    add all

--- Enotes_Management_System/settings.py
+++ Enotes_Management_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=#p#1$e9vnd8_ke*nzr+=0(o7b49t2=3yf@tr#(f-7tyn%u5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['enotessystem.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['enotessystem.herokuapp.com/','localhost']
 # ALLOWED_HOSTS = []
 
 
