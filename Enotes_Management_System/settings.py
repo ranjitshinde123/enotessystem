1f0d9681e7e6b01a1c049d4786cfd9fbabf7a02f
Ranjit Shinde <ranjitshinde9404@gmail.com>
2022-08-21 18:04:20 +0530

    i did add procfile

--- Enotes_Management_System/settings.py
+++ Enotes_Management_System/settings.py
@@ -122,12 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
-import os
-#
-STATICFLES_DIR=[
-    os.path.join(BASE_DIR,"static")
-]
+# STATIC_ROOT=os.path.join(BASE_DIR,"static")
+# STATICFILES_DIR=[
+#     os.path.join(BASE_DIR,"static")
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
