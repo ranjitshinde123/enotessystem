c05af90b8e7a22dc2d2df38d6293a3b5ddbfc801
ranjitshinde123 <87406747+ranjitshinde123@users.noreply.github.com>
2022-08-25 15:02:13 +0530

    Update settings.py

--- Enotes_Management_System/settings.py
+++ Enotes_Management_System/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,7 +133,7 @@ STATICFILES_DIR=[
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
