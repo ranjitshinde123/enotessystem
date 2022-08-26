1273c07d96b8767dad20c983c7bed7f372967e32
ranjitshinde123 <87406747+ranjitshinde123@users.noreply.github.com>
2022-08-26 18:54:24 +0530

    Update settings.py

--- Enotes_Management_System/settings.py
+++ Enotes_Management_System/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIR=[
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
